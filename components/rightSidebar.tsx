91e23feccc1aef1c541741b6c330188725ac2e90
rachels-archive <rachelhui@protonmail.com>
2024-09-30 00:34:42 +0800

    Create Right Sidebar component

---
+++ components/rightSidebar.tsx
@@ -0,0 +1,8 @@
+export default function RightSidebar() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-end gap-6 text-textLight">
+      <p className="text-sm rotate-90 text-textGreen mb-32">Like what you see? Let’s connect for opportunities!</p>
+      <div className="w-[2px] h-32 bg-textDark"></div>
+    </div>
+  );
+}
