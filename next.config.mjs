cf4ea85584e085ac7150c759a00ebedbe674633e
rachels-archive <79963756+rachels-archive@users.noreply.github.com>
2024-10-01 10:59:45 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/portfolio', // Replace with your repository name
+  assetPrefix: '/portfolio/',
+  distDir: 'out',
 };
 
 export default nextConfig;
