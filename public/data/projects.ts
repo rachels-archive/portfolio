6f36849a76bd3e2ca34b98c5e5364c09818c1b6f
rachels-archive <79963756+rachels-archive@users.noreply.github.com>
2024-10-01 12:45:43 +0800

    Update projects.ts

--- public/data/projects.ts
+++ public/data/projects.ts
@@ -4,7 +4,7 @@ const projects = [
     description:
       "An AI powered video caption generator powered by Deepgram AI video transcription API. Built with NextJS,Tailwind CSS, FFmpeg and Firebase Cloud Storage.",
     technologies: ["Next.js", "Tailwind CSS", "Firebase", "Tailwind CSS", "Deepgram API"],
-    imageUrl: "/images/project1.png",
+    imageUrl: "/portofolio/images/project1.png",
     projectUrl: "https://ai-caption-generator-woad.vercel.app",
     githubUrl: "https://github.com/rachels-archive/AI-Caption-Generator",
     type: "featured",
@@ -14,7 +14,7 @@ const projects = [
     description:
       "A movies and TV shows aggregate website that allows users to browse, search, and filter a vast library of movies and TV shows. Built with Angular v17, TMDB API, and design based on Netflix.",
     technologies: ["Angular", "Typescript", "SCSS", "TMDB API"],
-    imageUrl: "/images/project2.png",
+    imageUrl: "portofolio/images/project2.png",
     projectUrl: "https://angular17-movies-app.netlify.app",
     githubUrl: "https://github.com/rachels-archive/NgFlix",
     type: "featured",
