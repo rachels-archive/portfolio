ba3759497c6b596c38a6d1dd65b00c1cf7fa88c9
rachels-archive <79963756+rachels-archive@users.noreply.github.com>
2024-10-01 10:05:48 +0800

    Fix unescaped single quotes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
       className="max-w-containerSmall mx-auto px-4 py-10 lgl:py-32 flex flex-col gap-4 items-center justify-center"
     >
       <p className="font-titleFonttext-lg text-textGreen font-semibold flex items-center tracking-wide ">
-        04. What's next?
+        04. What&apos;s next?
       </p>
       <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
       <p className="max-w-[600px] text-center text-textDark">
@@ -16,7 +16,7 @@ export default function Contact() {
       </p>
       <a href={`mailto:${socials.email}`}>
         <button className="border-2 border-textGreen rounded-lg py-3 px-5 text-textGreen hover:bg-hoverColor">
-          Let's Connect!
+          Let&apos;s Connect!
         </button>
       </a>
     </div>
