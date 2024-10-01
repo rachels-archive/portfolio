cfe9f96de33f560e99f8c139126cc0deec4a0600
rachels-archive <79963756+rachels-archive@users.noreply.github.com>
2024-10-01 13:22:31 +0800

    Update projects.ts

--- public/data/projects.ts
+++ public/data/projects.ts
@@ -4,7 +4,7 @@ const projects = [
     description:
       "An AI powered video caption generator powered by Deepgram AI video transcription API. Built with NextJS,Tailwind CSS, FFmpeg and Firebase Cloud Storage.",
     technologies: ["Next.js", "Tailwind CSS", "Firebase", "Tailwind CSS", "Deepgram API"],
-    imageUrl: "./portofolio/images/project1.png",
+    imageUrl: "/portofolio/images/project1.png",
     projectUrl: "https://ai-caption-generator-woad.vercel.app",
     githubUrl: "https://github.com/rachels-archive/AI-Caption-Generator",
     type: "featured",
@@ -14,7 +14,7 @@ const projects = [
     description:
       "A movies and TV shows aggregate website that allows users to browse, search, and filter a vast library of movies and TV shows. Built with Angular v17, TMDB API, and design based on Netflix.",
     technologies: ["Angular", "Typescript", "SCSS", "TMDB API"],
-    imageUrl: "./portofolio/images/project2.png",
+    imageUrl: "/portofolio/images/project2.png",
     projectUrl: "https://angular17-movies-app.netlify.app",
     githubUrl: "https://github.com/rachels-archive/NgFlix",
     type: "featured",
@@ -24,7 +24,7 @@ const projects = [
     description:
       "A password manager application that enables users to generate, store, and manage their passwords, developed as my final project for Harvard's CS50 course.",
     technologies: ["HTML", "CSS", "JavaScript", "jQuery", "Python", "Flask", "SQL"],
-    imageUrl: "/images/project5.png",
+    imageUrl: "/portofolio/images/project5.png",
     projectUrl: "https://password-pal.onrender.com",
     githubUrl: "https://github.com/rachels-archive/password-pal",
     type: "featured",
@@ -33,7 +33,7 @@ const projects = [
     title: "Paint App Lite",
     description: "A simple version of the Microsoft Paint app built with HTML5 Canvas and Vanilla JavaScript.",
     technologies: ["HTML5", "CSS", "JavaScript"],
-    imageUrl: "/images/project3.png",
+    imageUrl: "/portofolio/images/project3.png",
     projectUrl: "https://rachels-archive.github.io/Paint-Lite",
     githubUrl: "https://github.com/rachels-archive/Paint-Lite",
     type: "archived",
@@ -42,7 +42,7 @@ const projects = [
     title: "Accessibility-Friendly Job Portal",
     description: "A full stack job-portal focused on accessibility and catered for job seekers with disabilities.",
     technologies: ["Angular", "Tailwind CSS", "Express.JS", "Firebase"],
-    imageUrl: "/images/project4.png",
+    imageUrl: "/portofolio/images/project4.png",
     projectUrl: "https://accessability.app/",
     githubUrl: "https://github.com/Spimy/AccessAbility",
     type: "archived",
@@ -52,7 +52,7 @@ const projects = [
     description:
       "A web app built with Python and Streamlit, that automates the task of head counting by leveraging the MTCNN algorithm.",
     technologies: ["Python", "Streamlit"],
-    imageUrl: "/images/project6.png",
+    imageUrl: "/portofolio/images/project6.png",
     projectUrl: "https://ml-headcounter.streamlit.app/",
     githubUrl: "https://github.com/rachels-archive/mtcnn-head-counter",
     type: "archived",
